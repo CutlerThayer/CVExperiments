ff323d374271970866382747782f02a4a14a996b
Cutler <cutlerthayer@gmail.com>
2020-05-16 12:05:41 -0500

    Following tutorial to learn basics

---
+++ experiment1.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+img = cv.imread('lena.jpg', 1)
+
+print(img)
+
+cv.imshow('image', img)
+k = cv.waitKey(0)
+
+if k == 27:
+    cv.destroyAllWindows()
+elif k == ord('s'):
+    cv.imwrite('lena_copy.png', img)
+    cv.destroyAllWindows()
