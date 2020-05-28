3ee8131daa5dcc7bb8eec1c3c63a045b1a6ca3f1
Cutler <cutlerthayer@gmail.com>
2020-05-28 13:23:26 -0500

    Learning about adaptive thresholding

--- thresholding/ImageThresholding.py
+++ thresholding/ImageThresholding.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('gradient.png',0)
+img = cv.imread('../gradient.png', 0)
 _, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
 _, th2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
 _, th3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
