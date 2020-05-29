02e5486601fe987dae384667a9e10ca74794fd3e
Cutler <cutlerthayer@gmail.com>
2020-05-29 11:36:16 -0500

    Learning about different morphological transformations

--- MorphilogicalTransformations.py
+++ MorphilogicalTransformations.py
@@ -3,12 +3,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv.imread('smarties.png', cv.IMREAD_GRAYSCALE)
+_, mask = cv.threshold(img, 220, 255, cv.THRESH_BINARY_INV)
 
-titles = ['image']
-images = [img]
+kernel = np.ones((5,5), np.uint8)
 
-for i in range(1):
-    plt.subplot(1, 1, i+1), plt.imshow(images[i], 'gray')
+dilation = cv.dilate(mask, kernel, iterations=2)
+erosion = cv.erode(mask, kernel, iterations=1)
+opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+closing = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+mg = cv.morphologyEx(mask, cv.MORPH_GRADIENT, kernel)
+th = cv.morphologyEx(mask, cv.MORPH_TOPHAT, kernel)
+
+
+titles = ['image', 'mask', 'dilation', 'erosion', 'opening', 'closing', 'gradient', 'tophat']
+images = [img, mask, dilation, erosion, opening, closing, mg, th]
+
+for i in range(8):
+    plt.subplot(2, 4, i+1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
